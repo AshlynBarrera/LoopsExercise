f73465631aeafb32ab5ac73f141772e376562f48
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-22 10:53:37 -0600

    Finished inital exercise, includes loops.

---
+++ script.js
@@ -0,0 +1,39 @@
+
+//1
+nums = [1,2,3,4,5,6,7]
+for ( i of nums){
+    console.log(i);
+}
+
+
+console.log("\n");
+//2
+for (i = 5 ; i < 26; i++ ){
+    console.log(i);
+
+}
+
+//3a
+const wizards = [
+    "Harry potter",
+    "Hermonie Granger",
+    "Ron Weasley"
+]
+
+console.log("\n");
+// 3b 
+for (i of wizards){
+    console.log(i);
+  }
+
+//4a 
+let harryPotterMovies = 0;
+
+// 4b
+while(harryPotterMovies < 8) {
+    harryPotterMovies++;  
+}
+
+console.log("\n");
+//4c 
+console.log(harryPotterMovies);
